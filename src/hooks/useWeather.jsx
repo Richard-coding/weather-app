552a9565c314ee09d058c5cdc18ec4f95772fddf
Richard <richardcoding@outlook.com>
2026-05-07 14:20:57 -0300

    refactor: move API calls from hook

--- src/hooks/useWeather.jsx
+++ src/hooks/useWeather.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
+import { weatherApi } from "../services/weatherApi";
 
 const defaultCity = {
   name: "Osasco",
@@ -23,15 +24,11 @@ const useWeather = () => {
     try {
       setLoading(true);
       setError(null);
-      const response = await fetch(
-        `https://api.open-meteo.com/v1/forecast?latitude=${city.latitude}&longitude=${city.longitude}&daily=temperature_2m_max,temperature_2m_min,rain_sum,showers_sum,snowfall_sum,precipitation_sum,weather_code&hourly=apparent_temperature,rain,showers,snowfall,snow_depth,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,wind_speed_10m,weather_code&wind_speed_unit=${unit.windspeed}&temperature_unit=${unit.temperature}&precipitation_unit=${unit.precipitation}`,
-      );
+      const { response, json } = await weatherApi(city, unit);
 
       if (!response.ok) {
         throw new Error("Failed to fetch weather data");
       }
-
-      const json = await response.json();
       setSelected(city);
       setData(json);
 
