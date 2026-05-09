fe39ec80812553fb3a4123568d27fde7c542cf3d
Richard-coding <richardcoding@outlook.com>
2026-05-09 13:31:38 -0300

    fix: adjust responsive layout Weather-info

--- src/components/weather/WeatherInfo.jsx
+++ src/components/weather/WeatherInfo.jsx
@@ -16,17 +16,17 @@ const WeatherInfo = () => {
 
   return (
     <section>
-      <div className="bg-mobile md:bg-desktop text-center bg-cover flex flex-wrap items-center md:justify-between gap-4 px-6 py-20 rounded-16">
-        <div className="text-start mx-auto md:mx-0">
+      <div className="bg-mobile md:bg-desktop bg-cover gap-4 px-6 py-20 rounded-16 items-center grid grid-cols-1 sm:grid-cols-2  ">
+        <div className="text-center sm:text-start">
           <p className="text-preset-4 mb-3">
             {selected?.name}, {selected?.admin1}
           </p>
           <p className="text-preset-6 opacity-80">{today}</p>
         </div>
-        <div className="flex items-center gap-5">
+        <div className="flex gap-5 justify-center items-center sm:justify-end">
           <Icons
             weatherCode={data?.current?.weather_code}
-            className="w-[120px] h-[120px]"
+        className="w-[104px] h-[104px]" 
           />
           <p className="text-preset-1 semi-bold">
             {data?.current?.temperature_2m.toFixed(0)} º
